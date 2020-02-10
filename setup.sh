d8e069bc3166865750ef5b540829ea40d559166b
Connor Clark <cjamcl@gmail.com>
2020-02-09 17:54:34 -0800

    initial

---
+++ setup.sh
@@ -0,0 +1,48 @@
+if ! [ -x "$(command -v brew)" ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Terminal
+if ! [ -x "$(command -v zsh)" ]; then
+  brew install zsh
+  brew cask install iterm2
+  chsh -s /bin/zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  mkdir -p ~/terminal
+  cd ~/terminal
+  curl -O https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Dracula.itermcolors
+
+  echo '. ~/.zshrc-cjamcl' >> ~/.zshrc
+
+  cd ~
+fi
+
+cp .zshrc-cjamcl ~/.zshrc-cjamcl
+
+# https://superuser.com/questions/71588/how-to-syntax-highlight-via-less
+brew install highlight
+
+# Node
+if ! [ -x "$(command -v nvm)" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+fi
+
+if ! [ -x "$(command -v yarn)" ]; then
+  brew install yarn
+fi
+
+yarn global add bundle-buddy
+
+# Python
+
+if ! [ -x "$(command -v pip)" ]; then
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python get-pip.py --user
+fi
+
+# Code
+if ! [ -x "$(command -v code)" ]; then
+  brew install visual-studio-code
+fi
+cp vscode-settings.json ~/Library/"Application Support"/Code/User/settings.json
